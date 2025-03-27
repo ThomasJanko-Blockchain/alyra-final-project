665a5461cc404a19336877d2b856010a3e0db720
Thomas Janko <thomas78125@gmail.com>
2025-03-27 20:25:05 +0100

    footer position

--- frontend/src/components/layouts/Footer.jsx
+++ frontend/src/components/layouts/Footer.jsx
@@ -1,7 +1,7 @@
 
 export default function Footer() {
   return (
-    <footer className="border-t border-gray-100 border-opacity-50 py-8 w-full flex justify-around fixed bottom-0 items-center p-6">
+    <footer className="border-t border-gray-100 border-opacity-50 py-8 w-full flex justify-around items-center p-6 mt-20">
         <p>© SerieCoin 2025, All Rights Reserved</p>
         <div className="flex items-center gap-x-4">
             <span>Privacy Policy</span>
